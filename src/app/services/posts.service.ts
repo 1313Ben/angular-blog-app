8b91e67655e06257ee0e0ebf456745b8c9b8460c
Ravindra Benadict Asirvatham <ravindra_github@bluewin.ch>
2023-03-07 14:30:58 +0100

    Load latest post query

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -17,7 +17,19 @@ export class PostsService {
           const id = a.payload.doc.id;
           return {id, data}
         })
-    })
+      })
     )
   }
+
+  loadLatestPosts() {
+    return this.afs.collection('posts', ref => ref.orderBy('createdAt')).snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return {id, data}
+        })
+      })
+    )
+  } 
 }
