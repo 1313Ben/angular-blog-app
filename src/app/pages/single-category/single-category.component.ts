b2b83de6c02f2241fc495724770dacd92e93a3b7
Ravindra Benadict Asirvatham <ravindra_github@bluewin.ch>
2023-03-08 13:39:59 +0100

    Post category filter

--- src/app/pages/single-category/single-category.component.ts
+++ src/app/pages/single-category/single-category.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-category',
   templateUrl: './single-category.component.html',
   styleUrls: ['./single-category.component.css']
 })
-export class SingleCategoryComponent {
+export class SingleCategoryComponent implements OnInit{
+
+  postsArray: Array<object> = []
+
+  constructor(private route: ActivatedRoute, private postService: PostsService) { }
+  
+  ngOnInit(): void {
+    this.route.params.subscribe(val => {
+      //console.log(val['id'])
+      this.postService.loadCategoryPosts(val['id']).subscribe(post => {
+        this.postsArray = post;
+     })
+   })
+  }
 
 }
