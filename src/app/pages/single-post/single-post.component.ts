3890a32a280ffcf8c40cabd30dd1faababdeb6b5
Ravindra Benadict Asirvatham <ravindra_github@bluewin.ch>
2023-03-08 17:22:37 +0100

    Single post component dynamic

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
   styleUrls: ['./single-post.component.css']
 })
-export class SinglePostComponent {
+export class SinglePostComponent implements OnInit{
+
+  postData: any;
+
+  constructor(private route: ActivatedRoute, private postService: PostsService) { }
+  
+  ngOnInit(): void {
+
+    this.route.params.subscribe(val => {
+      this.postService.loadOnePost(val['id']).subscribe(post => {
+        //console.log(post)
+        this.postData = post;
+      })
+    })
+  }
 
 }
